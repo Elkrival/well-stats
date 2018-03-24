7aef66e886c6f5ac9a6b119b9661a8a44970d6b8
Ivan Trujillo <krival1987@gmail.com>
2018-03-24 11:01:28 -0400

    merged about pages and business value pages together

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,44 +1,68 @@
 import React from 'react';
 import logo from '../../img/wellstat_logo.png'
+
+
 const About = () =>{
  return(
   <section className="section-about">
-   <div className="u-center-text u-margin-bottom-big">
+   <div className="u-center-text u-margin-bottom-small">
     <h2 className="heading-secondary">
-     Alone we can do so little; together we can do so much. – Hellen Keller
+     We Are Wellstats
     </h2>
    </div>
-   <div className="row">
-    <div className="story">
-     <figure className="story__shape">
-      <img src={ logo } alt="logo" className="story__img"/>
-      <figcaption className="story__caption">Comprehensive</figcaption>
-     </figure>
-     <div className="story__text">
-      <h3 className="heading-tertiary margin-bottom-small">
-       Comprehensive Services
-      </h3>
-      <p> WellStats understand that in the world of Human and Health services it is always a juggling act between massive budget cuts, increased regulartions, and porivinding the most vulnerable populations with exceptaional care. <a href="#here" className="btn-text">Take the first step &rArr;</a></p>
+    <div className="row">
+   <div className="col-1-of-2">
+    <h3 className="heading-tertiary u-margin-bottom-small">
+     COMPREHENSIVE SERVICES
+    </h3>
+    <p className="paragraph">
+     WellStats understand that in the world of Human and Health services it is always a juggling act between massive budget cuts, increased regulartions, and porivinding the most vulnerable populations with exceptaional care.
+    </p>
+    <h3 className="heading-tertiary u-margin-bottom-small">
+     Chew Toys!
+    </h3>
+     <p className="paragraph">
+      Pet Food pet supplies gimme five puppy cage food feathers food heel feathers running pet gate walk lazy dog Spike. Good Boy park lazy dog walk kibble Scooby snacks licks canary.
+     </p>
+     <a href="3" className="btn-text">
+      Take the First Step &rArr;
+     </a>
+   </div>
+   <div className="col-1-of-2">
+    <div className="composition">
+     <div className="composition__card composition__card--c1 card ">
+      <div className="card__side card__side--front">
+       <div className="card__picture card__picture--1">
+        &nbsp;
+       </div>
+      <h4 className="card__heading">
+       <span className="card__heading-span card__heading-span--1"> Integrated and Sustainable Systems</span>
+      </h4>
+     </div>
+    </div>
+    <div className="composition__card composition__card--c2 card">
+      <div className="card__side card__side--front">
+       <div className="card__picture card__picture--2">
+        &nbsp;
+       </div>
+       <h4 className="card__heading">
+        <span className="card__heading-span card__heading-span--2"> Effective and Efficient Workflows</span>
+       </h4>
+      </div>
+     </div>
+ <div className="composition__card composition__card--c3 card">
+      <div className="card__side card__side--front">
+       <div className="card__picture card__picture--3">
+        &nbsp;
+       </div>
+       <h4 className="card__heading">
+        <span className="card__heading-span card__heading-span--3"> Data Driven Predictive Analysis</span>
+       </h4>
+      </div>
      </div>
     </div>
    </div>
-   <div className="row">
-    <div className="story">
-     <figure className="story__shape">
-      <img src={ logo } alt="logo" className="story__img"/>
-      <figcaption className="story__caption">For you.</figcaption>
-     </figure>
-     <div className="story__text">
-      <h3 className="heading-tertiary margin-bottom-small">
-       With you in mind.
-      </h3>
-      <p>
-       Deisgned by professionals in the industry, WellStats is here to partner with your agency on transformative technology solutions that fit your needs and help your agency thrive.
-       <a href="#here" className="btn-text">Take the first step &rArr;</a>
-      </p>
-     </div>
-    </div>
-   </div>
+  </div>
   </section>
  )
 }
